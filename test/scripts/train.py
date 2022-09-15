78538f630564ccddefdad4f9535c6e721bb266f5
hepller <xpneomix@gmail.com>
2022-09-16 01:11:23 +0300

    Обновление зависимостей.

--- test/scripts/train.py
+++ test/scripts/train.py
@@ -8,7 +8,7 @@ from core.trainer import Trainer
 if __name__ == "__main__":
 	arg_parser: ArgumentParser = ArgumentParser()
 
-	arg_parser.add_argument("-ec", "--epochs-count", type=int, help="Number of epochs (default: 200)", default=300)
+	arg_parser.add_argument("-ec", "--epochs-count", type=int, help="Number of epochs (default: 200)", default=200)
 	arg_parser.add_argument("-bs", "--batch-size", type=int, help="Batch size (default: 64)", default=64)
 	arg_parser.add_argument("-o", "--overtrain", type=bool, help="Further training of the existing model (default: false)", default=False)
 	arg_parser.add_argument("-md", "--model-dir", type=str, help="The path to the model directory (default: ../model)", default="../model")
